e9d6fdc918f64b8abd743a8a6be0e4d7b4eca70a
ErikV121 <ErikVasquez121@gmail.com>
2025-04-06 23:41:17 -0400

    Created custom responseDto and added util

--- account-service/account-service/src/main/java/com/ErikV121/account_service/dto/ResponseDto.java
+++ account-service/account-service/src/main/java/com/ErikV121/account_service/dto/ResponseDto.java
@@ -1,5 +1,47 @@
 package com.ErikV121.account_service.dto;
 
-public record ResponseDto(
-        String statusCode,
-        String statusMessage) {}
+public class ResponseDto<T> {
+    private String status;
+    private String message;
+    private T data;
+    private Object metadata;
+
+    public ResponseDto(String status, String message, T data, Object metadata) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+        this.metadata = metadata;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Object getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(Object metadata) {
+        this.metadata = metadata;
+    }
+}
