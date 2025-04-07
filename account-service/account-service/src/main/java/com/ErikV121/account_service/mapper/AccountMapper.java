3b38d05866a7ec5a57673276652300a4f94693b1
ErikV121 <ErikVasquez121@gmail.com>
2025-04-06 23:45:08 -0400

    Added small fix: mapping

--- account-service/account-service/src/main/java/com/ErikV121/account_service/mapper/AccountMapper.java
+++ account-service/account-service/src/main/java/com/ErikV121/account_service/mapper/AccountMapper.java
@@ -4,9 +4,11 @@ import com.ErikV121.account_service.dto.accountDto.AccountRequestDto;
 import com.ErikV121.account_service.dto.accountDto.AccountResponseDto;
 import com.ErikV121.account_service.entity.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
+    @Mapping(target = "customerId", source = "customer.customerId")
     AccountResponseDto toAccountResponseDto(Account account);
 
     Account toAccount(AccountRequestDto accountRequestDto);
--- account-service/account-service/src/main/java/com/ErikV121/account_service/mapper/CustomerMapper.java
+++ account-service/account-service/src/main/java/com/ErikV121/account_service/mapper/CustomerMapper.java
@@ -2,12 +2,18 @@ package com.ErikV121.account_service.mapper;
 
 import com.ErikV121.account_service.dto.customerDto.CustomerRequestDto;
 import com.ErikV121.account_service.dto.customerDto.CustomerResponseDto;
+import com.ErikV121.account_service.dto.customerDto.NewCustomerRequestDto;
 import com.ErikV121.account_service.entity.Customer;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
      CustomerResponseDto toCustomerResponseDto(Customer customer);
 
+     @Mapping(target = "ssn", source = "ssn")
+     Customer toCustomer(NewCustomerRequestDto newCustomerRequestDto);
+
+     @Mapping(target = "ssn", source = "ssn")
      Customer toCustomer(CustomerRequestDto customerRequestDto);
 }
