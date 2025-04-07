c80771312f8b6980bd52fb68995ace50be4ff592
ErikV121 <ErikVasquez121@gmail.com>
2025-04-07 00:46:23 -0400

    Added small fix: Mapping for accountId

--- account-service/account-service/src/main/java/com/ErikV121/account_service/mapper/AccountMapper.java
+++ account-service/account-service/src/main/java/com/ErikV121/account_service/mapper/AccountMapper.java
@@ -8,6 +8,7 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
+    @Mapping(target = "accountId", source = "accountId")
     @Mapping(target = "customerId", source = "customer.customerId")
     AccountResponseDto toAccountResponseDto(Account account);
 
