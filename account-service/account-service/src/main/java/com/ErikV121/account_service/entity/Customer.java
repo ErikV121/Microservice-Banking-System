fd3747527ab78fee0be53bae602548b42f25e1d9
ErikV121 <ErikVasquez121@gmail.com>
2025-04-08 23:49:37 -0400

    Added small change, Customer account list

--- account-service/account-service/src/main/java/com/ErikV121/account_service/entity/Customer.java
+++ account-service/account-service/src/main/java/com/ErikV121/account_service/entity/Customer.java
@@ -2,6 +2,8 @@ package com.ErikV121.account_service.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -15,14 +17,17 @@ public class Customer extends BaseEntity {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "email")
+    @Column(name = "email", unique = true)
     private String email;
 
-    @Column(name = "mobile_number")
+    @Column(name = "mobile_number", unique = true)
     private String mobileNumber;
 
-    @Column(name = "social_security_number")
+    @Column(name = "social_security_number", unique = true)
     private String ssn;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Account> accounts = new ArrayList<>();
 
     public Customer() {}
 
@@ -63,5 +68,21 @@ public class Customer extends BaseEntity {
 
     public void setMobileNumber(String mobileNumber) {
         this.mobileNumber = mobileNumber;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
     }
 }
